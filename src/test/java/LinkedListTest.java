9dd0d0841ee08a27bafb488e3f03121ba23a94dd
kianoush97 <kianoush.abdal@gmail.com>
2021-03-20 14:46:04 +0100

    LinkedListTest done

--- src/test/java/LinkedListTest.java
+++ src/test/java/LinkedListTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -6,33 +7,61 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedListTest {
 
+    LinkedList linkedList;
+
+
     @BeforeEach
     void setUp() {
+        linkedList = new LinkedList<>();
+
 
     }
 
     @AfterEach
     void tearDown() {
+
     }
 
     @Test
     void isEmpty() {
-        //Todo: 0,1,many
+
+        assertTrue(linkedList.isEmpty());
     }
 
     @Test
     void size() {
-        //Todo: 0,1,many
+
+        linkedList.addToFront(7);
+        linkedList.addToFront(5);
+        assertEquals(2,linkedList.size());
+
     }
 
     @Test
-    void addToFront() {
-        //Todo: 1
+    void addToFront() throws EmptyListException
+    {
+
+        linkedList.addToFront(1);
+        linkedList.addToFront(3);
+        assertEquals(3, linkedList.removeFirst());
     }
 
     @Test
-    void removeFirst() {
-        //Todo: 1, Exception(Empty)
+    void removeFirst() throws EmptyListException
+    {
+        linkedList.addToFront(10);
+        linkedList.addToFront(5);
+        linkedList.removeFirst();
+       assertEquals( 10,linkedList.removeFirst());
     }
-    //TODO: KIANOUSH
+    @Test
+    void testExpectedException()
+    {
+        Assertions.assertThrows(EmptyListException.class, () -> {
+            linkedList.addToFront(20);
+            linkedList.removeFirst();
+            linkedList.removeFirst();
+        });
+    }
+
 }
